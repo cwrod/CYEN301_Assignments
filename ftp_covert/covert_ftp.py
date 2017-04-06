50d8eb589433e90f0f8ba2c90118a87798496e0b
Christopher Rodriguez <chrizrodz@gmail.com>
2017-04-05 22:25:07 -0500

    Indentation Fixes

--- ftp_covert/covert_ftp.py
+++ ftp_covert/covert_ftp.py
@@ -18,35 +18,35 @@ args = parser.parse_args()
 
 
 def ASCII(bin_string,encode):
-        ret_str = ""
-        for i in range(0,len(bin_string)/encode):
-                n_byte = int(bin_string[i*encode:(i+1)*encode],2)
-                if(n_byte==8):
-                        ret_str = ret_str[:-1]
-                elif(n_byte==9):
-                        ret_str = ret_str+"\t"
-                elif(n_byte==10):
-                        ret_str = ret_str+"\n"
-                elif(n_byte==13):
-                        ret_str = ret_str+"\r"
+	ret_str = ""
+	for i in range(0,len(bin_string)/encode):
+		n_byte = int(bin_string[i*encode:(i+1)*encode],2)
+		if(n_byte==8):
+			ret_str = ret_str[:-1]
+		elif(n_byte==9):
+			ret_str = ret_str+"\t"
+		elif(n_byte==10):
+			ret_str = ret_str+"\n"
+		elif(n_byte==13):
+			ret_str = ret_str+"\r"
 		elif(n_byte==0):
 			ret_str = ret_str
-                elif(n_byte>31 and n_byte<127):
-                        ret_str = ret_str+ASCII_TABLE[n_byte-32]
-                else:
-                        return 0
-        return ret_str
+		elif(n_byte>31 and n_byte<127):
+			ret_str = ret_str+ASCII_TABLE[n_byte-32]
+		else:
+			return 0
+	return ret_str
 
 def decode_bin(bin_string):
-        if(len(bin_string) % (7*8) == 0):
-                if(ASCII(bin_string,7)==0):
-                        return ASCII(bin_string,8)
-                else:
-                        return ASCII(bin_string,7)
-        elif(len(bin_string) % 7 == 0):
-                return ASCII(bin_string,7)
-        elif(len(bin_string) % 8 == 0):
-                return ASCII(bin_string,8)
+	if(len(bin_string) % (7*8) == 0):
+		if(ASCII(bin_string,7)==0):
+			return ASCII(bin_string,8)
+		else:
+			return ASCII(bin_string,7)
+	elif(len(bin_string) % 7 == 0):
+		return ASCII(bin_string,7)
+	elif(len(bin_string) % 8 == 0):
+		return ASCII(bin_string,8)
 
 
 
